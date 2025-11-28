7356db6dd9eaa59de5da51e8d23b9c94e5004d8b
danielleonsaldana <danieleonsaldana@gmail.com>
2025-11-28 08:24:07 -0600

    Agregar addons

--- xas_distributions/models/__init__.py
+++ xas_distributions/models/__init__.py
@@ -1,11 +1,4 @@
 # -*- coding: utf-8 -*-
-################################################################
-#
-# Author: Analytica Space
-# Coder: Giovany Villarreal (giv@analytica.space)
-#
-################################################################
-
 from . import res_company
 from . import purchase
 from . import stock
